f835d998b939109a69804b558e1abad0e84b752f
Svante Pålsson <svantepalsson1987@gmail.com>
2022-08-23 21:34:28 +0200

    Frontpage nu färdig med titel, länk, bild, text.

--- public/wp-content/themes/svantestheme/front-page.php
+++ public/wp-content/themes/svantestheme/front-page.php
@@ -4,6 +4,9 @@ get_header();
 
 $title = get_field('title');
 $description = get_field('description');
+$link = get_field('link');
+$get_image = get_field('image');
+$image = $get_image['sizes']['large'];
 
 ?>
   <section id="frontpage">
@@ -21,9 +24,19 @@ $description = get_field('description');
         <h1><?php echo $title; ?></h1>
     <?php endif;
 
+    if($link) : ?>
+    <a href="<?php echo $link['url']; ?>" >LINK LINK LINK</a>
+    <?php endif;
+
+    if($image) : ?>
+    <img src="<?php echo $image; ?>">
+    <?php endif;
+
     if($description): ?>
     <p><?php echo nl2br($description); ?></>
     <?php endif; ?>
+    
+      
     </div>
    </section>
 <?php
