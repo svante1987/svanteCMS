2d3db45dd2d0b823647ea05d685601261121155a
Svante Pålsson <svantepalsson1987@gmail.com>
2022-08-23 14:10:54 +0200

    har lagt till footer och header php filerna

---
+++ public/wp-content/themes/svantestheme/footer.php
@@ -0,0 +1,3 @@
+</body>
+<p> hej footer </p>
+</html>
--- public/wp-content/themes/svantestheme/header.php
+++ public/wp-content/themes/svantestheme/header.php
@@ -6,10 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Awesome Site</title>
-    <link rel="stylesheet" href="/wp-content/themes/svantestheme/style.css">
+    <link rel="stylesheet" href="/wp-content/themes/mycustomtheme/style.css">
 </head>
-
+<p> hej header </p>
 <body>
-<?php get_header();echo "Hello World 222";get_footer();?>
-</body>
-</html>
